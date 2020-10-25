3985c4864a0bf4dc3a817f94557c1e433e82e759
Mateusz Choma <kontakt@coderoad.pl>
2020-10-25 12:20:26 +0100

    .then and .catch methods

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // never resolving promise – all time in pending state
-const promise1 = new Promise(() => {})
+const promise1 = new Promise(() => { })
 console.log(promise1)
+promise1.then((value) => {
+    console.log('promise1 then callback', value)
+})
+promise1.catch((error) => {
+    console.log('promise1 catch callback', error)
+})
 
 // resolved / fulfilled promise
 const promise2 = new Promise((resolve, reject) => {
@@ -9,6 +15,12 @@ const promise2 = new Promise((resolve, reject) => {
     // reject()
 })
 console.log(promise2)
+promise2.then((value) => {
+    console.log('promise2 then callback', value)
+})
+promise2.catch((error) => {
+    console.log('promise2 catch callback', error)
+})
 
 // rejected promise
 const promise3 = new Promise((resolve, reject) => {
@@ -17,3 +29,9 @@ const promise3 = new Promise((resolve, reject) => {
     // resolve()
 })
 console.log(promise3)
+promise3.then((value) => {
+    console.log('promise3 then callback', value)
+})
+promise3.catch((error) => {
+    console.log('promise3 catch callback', error)
+})
